0aa9c6609cb723494bb0452e9f63836de2017d3c
Juanes1218 <juanestebanochoa93@gmail.com>
2024-01-25 09:36:46 -0500

    xde

---
+++ dia1/main.py
@@ -0,0 +1,50 @@
+##----------------------------
+##---- EJERCICIO1 ----
+##----------------------------
+## IMPRESIÓN EN CONSOLA 
+print("hola mundo") 
+
+# ----DATOS PRIMITIVOS ----
+# 1. String
+Texto = "campus"
+print(Texto)
+# 2. Int
+NumeroEntero = 3
+print(NumeroEntero)
+# 3. Double 
+NumeroDecimalLargo = 3.123124313
+print(NumeroDecimalLargo) 
+# 4. Float 
+Numerodecimal = 3.1
+print(Numerodecimal) 
+# 5. Boolean
+booleano = True
+print(booleano) 
+
+# ----entradas parte del usuario -----
+EntradaUsuario = input ("ingrese tu nombre")
+numeroFinal = (EntradaUsuario)
+print ("tu nombre es: ", numeroFinal)
+
+# ----entradas partes del usuario con definicion de tipo de dato primitivo ----
+EntradaUsuarioNumero = input ("ingrese tu edad")
+numeroFinal = int(EntradaUsuarioNumero)
+print ("tu edad es: ", EntradaUsuarioNumero) 
+
+# ----ciclos -----
+
+# ----ciclo for -----
+for i in range (5,10,2): #for contador in range (desde, hasta, pasos)
+    print(i)
+    
+# ----ciclo while ----
+booleanito = True
+while booleano == True: #while condicion_a_cumplir:
+    print("sigo vivo")
+    booleanito = False
+    
+    
+# ----condicionales-----
+
+
+## DESAROLLADO POR JUAN ESTEBAN OCHOA CORTES - 1091358460
