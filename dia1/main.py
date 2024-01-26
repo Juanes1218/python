d2ff75c7fffb7e0434c89c44cae3d0b897dad4f8
Juanes1218 <juanestebanochoa93@gmail.com>
2024-01-26 06:59:19 -0500

    trabajo en clase

--- dia1/main.py
+++ dia1/main.py
@@ -34,17 +34,70 @@ print ("tu edad es: ", EntradaUsuarioNumero)
 # ----ciclos -----
 
 # ----ciclo for -----
+
 for i in range (5,10,2): #for contador in range (desde, hasta, pasos)
     print(i)
     
 # ----ciclo while ----
+
 booleanito = True
-while booleano == True: #while condicion_a_cumplir:
+while booleanito == True: #while condicion_a_cumplir:
     print("sigo vivo")
     booleanito = False
     
-    
-# ----condicionales-----
+# ----condicionales-----#
+
+puntuacion=input("ingrese un numero")
+masato = int (puntuacion) 
+
+if masato > 100: 
+    print ("puntuacion es mayor")
+
+else:
+    print ("no es mayor")
+
+##----Con parametros y con retorno----##
+
+def suma(a, b):
+    return a + b
+
+resultado_suma = suma(3, 5)
+print(resultado_suma)
+
+
+##----sin parametros y sin retorno----##
+
+def saludar():
+    print("¡Hola!")
+
+saludar()
+
+
+##----con parametros y sin retorno----##
+
+def imprimir_multiplicacion(x, y):
+    resultado = x * y
+    print(f"El resultado es: {resultado}")
+
+imprimir_multiplicacion(4, 6)
+
+#----sin parametros y con retorno----##
+
+def obtener_numero():
+    return 42
+
+resultado_numero = obtener_numero()
+print(resultado_numero)
+
+##----arreglos----##
+numeros = [1, 2, 3, 4, 5]
+primer_elemento = numeros[0]
+ultimo_elemento = numeros[-1]
+numeros[2] = 10
+numeros.append(6)
+print("Lista completa:", numeros)
+print("Primer elemento:", primer_elemento)
+print("Último elemento:", ultimo_elemento)
 
 
 ## DESAROLLADO POR JUAN ESTEBAN OCHOA CORTES - 1091358460
